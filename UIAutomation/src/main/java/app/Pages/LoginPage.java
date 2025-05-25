34a8277dfadb34b23b0260a5f2ed91649c17c070
arvind7898 <avi4pune@gmail.com>
2025-05-26 00:13:33 +0530

    Update LoginPage.java

--- UIAutomation/src/main/java/app/Pages/LoginPage.java
+++ UIAutomation/src/main/java/app/Pages/LoginPage.java
@@ -17,18 +17,18 @@ public class LoginPage {
 	private WebDriver driver;
 	ElementUtil util = new ElementUtil(DriverFactory.getDriver());
 	
-	private By nickNameInputField = By.id("username");
-	private By passWordInputField = By.id("password");
-	private By loginButton = By.xpath("//button[contains(text(),'Log in')]");
-	private By wheelOfFortunePopUp = By.xpath("//div[@class='c-wheel__cta']");
-	private By collectBonusButton = By.xpath("//div[@class='c-streak__btn c-btn c-btn--primary c-btn--auto']");
-	private By collectButton = By.xpath("//a[@class='c-btn c-btn--primary c-dialog__cta']");
-	private By remindMeLaterRadioButton = By.xpath("//input[@type='radio' and @name='receiveEmail' and @value='false']");
-	private By marketingConsentNoRadioButton = By.xpath("//input[@type='radio' and @name='marketingConsent' and @value='false']");
-	private By confirmButton = By.xpath("//button[@type='submit']");
-	private By userProfile = By.xpath("//div[contains(text(),'Nick')]");
-	private By userMenu = By.xpath("//span[contains(normalize-space(text()), 'Hello')]");
-	private By logOutButton = By.xpath("//a[contains(text(),'Log out')]");
+	private By txtNickName = By.id("username");
+	private By txtPassWord = By.id("password");
+	private By btnLogin = By.xpath("//button[contains(text(),'Log in')]");
+	private By popupWheelOfFortune = By.xpath("//div[@class='c-wheel__cta']");
+	private By btnCollectBonus = By.xpath("//div[@class='c-streak__btn c-btn c-btn--primary c-btn--auto']");
+	private By btnCollect = By.xpath("//a[@class='c-btn c-btn--primary c-dialog__cta']");
+	private By radioBtnRemindMeLater = By.xpath("//input[@type='radio' and @name='receiveEmail' and @value='false']");
+	private By radioBtnMarketingConsentNo = By.xpath("//input[@type='radio' and @name='marketingConsent' and @value='false']");
+	private By btnConfirm = By.xpath("//button[@type='submit']");
+	private By lblUserProfile = By.xpath("//div[contains(text(),'Nick')]");
+	private By lblUserMenu = By.xpath("//span[contains(normalize-space(text()), 'Hello')]");
+	private By btnLogOut = By.xpath("//a[contains(text(),'Log out')]");
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
@@ -42,31 +42,31 @@ public class LoginPage {
 	public void firstLogin(String username, String pass ) throws InterruptedException{
 		Log.info("Logging first time with newly registered player !!!");
 		// Enter Nickname
-		util.waitForElementToBeVisible(nickNameInputField, 15);
-		util.doSendKeys(nickNameInputField, username);
+		util.waitForElementToBeVisible(txtNickName, 15);
+		util.doSendKeys(txtNickName, username);
 		// Enter Password
-   		util.waitForElementToBeVisible(passWordInputField, 15);
-		util.doSendKeys(passWordInputField, pass);
+   		util.waitForElementToBeVisible(txtPassWord, 15);
+		util.doSendKeys(txtPassWord, pass);
 		// Click on Login Button
-		util.waitForElementToBeClickable(loginButton, 15);
-		util.doClick(loginButton);
-		util.waitForElementToBeDisappear(loginButton, 10);
+		util.waitForElementToBeClickable(btnLogin, 15);
+		util.doClick(btnLogin);
+		util.waitForElementToBeDisappear(btnLogin, 10);
 		
 		// Click on Wheel of Fortune
-		util.waitForElementToBeVisible(wheelOfFortunePopUp,35);
-		util.doClick(wheelOfFortunePopUp);
+		util.waitForElementToBeVisible(popupWheelOfFortune,35);
+		util.doClick(popupWheelOfFortune);
 		TimeUnit.SECONDS.sleep(5);
 		System.out.println("Wheel of Fortune");
 		
 		// Click on Bonus button
-		util.waitForElementToBeVisible(collectBonusButton,35);
-		util.doClick(collectBonusButton);
+		util.waitForElementToBeVisible(btnCollectBonus,35);
+		util.doClick(btnCollectBonus);
 		TimeUnit.SECONDS.sleep(5);
 		System.out.println("collectBonusButton");
 		
 		// Click on Collect button
-		util.waitForElementToBeVisible(collectButton,25);
-		util.doClick(collectButton);
+		util.waitForElementToBeVisible(btnCollect,25);
+		util.doClick(btnCollect);
 		TimeUnit.SECONDS.sleep(5);
 		System.out.println("collectButton");
 		
@@ -75,29 +75,29 @@ public class LoginPage {
 		WebElement overlay = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".has-overlay")));
 		
 		// Click "receiveEmailNot now, remind me later." radio button
-		overlay.findElement(remindMeLaterRadioButton).click();
+		overlay.findElement(radioBtnRemindMeLater).click();
 		System.out.println("remindMeLaterRadioButton");
 		
 		// Click "No, I do not want to accept offers." radio button
 		TimeUnit.SECONDS.sleep(5);
-		overlay.findElement(marketingConsentNoRadioButton).click();
+		overlay.findElement(radioBtnMarketingConsentNo).click();
 		System.out.println("marketingConsentNoRadioButton");
 
 		// Click on Confirm Button
 		TimeUnit.SECONDS.sleep(5);
-		overlay.findElement(confirmButton).click();
+		overlay.findElement(btnConfirm).click();
 		System.out.println("confirmButton");
 		
 	}
 	// Click on User Profile at top right corner
 	public String clickOnProfile() {
 		Log.info("Click on User Profile at top right corner");
-		util.waitForElementToBeVisible(userProfile, 20);
-		util.doClick(userProfile);
+		util.waitForElementToBeVisible(lblUserProfile, 20);
+		util.doClick(lblUserProfile);
 		System.out.println("userProfile");
 		
-		util.waitForElementToBeVisible(userMenu, 10);
-		String helloMessage = util.doGetText(userMenu);
+		util.waitForElementToBeVisible(lblUserMenu, 10);
+		String helloMessage = util.doGetText(lblUserMenu);
 		System.out.println("Message in User Menu ---" +helloMessage);
 		return helloMessage;
 		
@@ -106,8 +106,8 @@ public class LoginPage {
 	public void clickOnLogout() {
 		Log.info("Player Logging Out !!!");
 		clickOnProfile();
-		util.waitForElementToBeVisible(logOutButton, 10);
-		util.doClick(logOutButton);
+		util.waitForElementToBeVisible(btnLogOut, 10);
+		util.doClick(btnLogOut);
 		System.out.println("User logged out successfully !!!!");
 	}
 }
